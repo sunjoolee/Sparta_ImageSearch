181d5a3082c2730687aea49c8b25a9d9c87fcb12
sunjoolee <sheisthepoem@naver.com>
2024-01-25 23:23:54 +0900

    Feat: ViewPager2

--- app/src/main/java/com/sparta/imagesearch/MainActivity.kt
+++ app/src/main/java/com/sparta/imagesearch/MainActivity.kt
@@ -2,10 +2,28 @@ package com.sparta.imagesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.sparta.imagesearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewpager.adapter = MyPagerAdapter(this)
     }
+
+    class MyPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity) {
+        override fun getItemCount(): Int = 2
+        override fun createFragment(position: Int): Fragment {
+            return if(position==0) SearchFragment() else FolderFragment()
+        }
+
+    }
+
 }
