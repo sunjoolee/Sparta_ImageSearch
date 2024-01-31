bc5aa1da68d1cb8a77edfc7d7d27a62fe18e0b9d
sunjoolee <sheisthepoem@naver.com>
2024-01-31 15:27:03 +0900

    Feat: show saved item in FolderFragment's RecyclerView

--- app/src/main/java/com/sparta/imagesearch/FolderFragment.kt
+++ app/src/main/java/com/sparta/imagesearch/FolderFragment.kt
@@ -1,59 +1,68 @@
 package com.sparta.imagesearch
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.sparta.imagesearch.data.Item
+import com.sparta.imagesearch.databinding.FragmentFolderBinding
+import com.sparta.imagesearch.recyclerView.GridSpacingItemDecoration
+import com.sparta.imagesearch.recyclerView.ItemAdapter
+import com.sparta.imagesearch.recyclerView.OnImageClickListener
+import com.sparta.imagesearch.util.fromDpToPx
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FolderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class FolderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class FolderFragment : Fragment(), OnImageClickListener {
+    private val TAG = "FolderFragment"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding:FragmentFolderBinding? = null
+    private val binding get() = _binding!!
+
+    //private lateinit var folderAdapter: FolderAdapter
+    private lateinit var itemAdapter: ItemAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_folder, container, false)
+        _binding = FragmentFolderBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FolderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FolderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initImageRecyclerView()
     }
+
+    private fun initImageRecyclerView(){
+        itemAdapter = ItemAdapter(MainActivity.savedItems)
+
+        itemAdapter.onImageClickListener = this@FolderFragment
+        binding.recyclerviewImage.run {
+            adapter = itemAdapter
+            addItemDecoration(GridSpacingItemDecoration(2, 16f.fromDpToPx()))
+        }
+    }
+
+    override fun onImageClick(item: Item) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onHeartClick(position: Int, item: Item) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onHeartLongClick(item: Item) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume")
+        super.onResume()
+        itemAdapter.notifyDataSetChanged()
+    }
+
 }
