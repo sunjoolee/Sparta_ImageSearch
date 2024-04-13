8c4cd78c0f0038c9123bcf56c9c5f85be91ac90e
sunjoolee <sheisthepoem@naver.com>
2024-04-14 05:17:53 +0900

    Refactor: Refactor ItemAdapter to ListAdapter
    Feat ItemDiffCallback

---
+++ app/src/main/java/com/sparta/imagesearch/view/ItemDiffCallback.kt
@@ -0,0 +1,27 @@
+package com.sparta.imagesearch.view
+
+import androidx.recyclerview.widget.DiffUtil
+import com.sparta.imagesearch.data.Item
+
+sealed interface ItemChangePayload {
+    data class FolderIdChange(val folderId: String) : ItemChangePayload
+}
+
+object ItemDiffCallback : DiffUtil.ItemCallback<Item>() {
+    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun getChangePayload(oldItem: Item, newItem: Item): Any? {
+        return when {
+            oldItem.folderId != newItem.folderId ->
+                ItemChangePayload.FolderIdChange(newItem.folderId)
+
+            else -> super.getChangePayload(oldItem, newItem)
+        }
+    }
+}
