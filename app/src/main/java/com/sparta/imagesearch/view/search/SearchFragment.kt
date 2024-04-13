3bcd5c306377f785af71592c7c988307913e35fe
sunjoolee <sheisthepoem@naver.com>
2024-04-14 05:19:02 +0900

    Refactor: Refactor SearchFragment to MVVM

---
+++ app/src/main/java/com/sparta/imagesearch/view/search/SearchFragment.kt
@@ -0,0 +1,109 @@
+package com.sparta.imagesearch.view.search
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.sparta.imagesearch.data.Item
+import com.sparta.imagesearch.databinding.FragmentSearchBinding
+import com.sparta.imagesearch.util.GridSpacingItemDecoration
+import com.sparta.imagesearch.view.ItemAdapter
+import com.sparta.imagesearch.view.OnHeartClickListener
+import com.sparta.imagesearch.util.fromDpToPx
+import com.sparta.imagesearch.viewmodel.SearchViewModel
+
+class SearchFragment : Fragment(), OnHeartClickListener {
+    private val TAG = "SearchFragment"
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    private val model by viewModels<SearchViewModel>()
+
+    private lateinit var itemAdapter: ItemAdapter
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initModelObserver()
+
+        initImageRecyclerView()
+        setSearchButtonOnClickListener()
+    }
+
+    private fun initModelObserver() {
+        with(model) {
+            keyword.observe(viewLifecycleOwner) { keyword ->
+                binding.etSearch.setText(keyword)
+            }
+
+            resultItems.observe(viewLifecycleOwner) { resultItems ->
+                Log.d(TAG, "resultItems.observe) savedItems.size: ${model.savedItems.value!!.size}")
+                itemAdapter.submitList(resultItems)
+            }
+        }
+    }
+
+    private fun initImageRecyclerView() {
+        itemAdapter = ItemAdapter()
+
+        itemAdapter.onHeartClickListener = this@SearchFragment
+        binding.recyclerviewImage.run {
+            adapter = itemAdapter
+            itemAnimator = null
+            addItemDecoration(GridSpacingItemDecoration(2, 16f.fromDpToPx()))
+        }
+    }
+
+    override fun onHeartClick(item: Item) {
+        model.saveItem(item)
+    }
+
+    private fun setSearchButtonOnClickListener() {
+        binding.btnSearch.setOnClickListener {
+            hideKeyboard(it)
+            smoothScrollToTop()
+
+            model.search(binding.etSearch.text.toString())
+        }
+    }
+
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager =
+            activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        view.clearFocus()
+    }
+
+    private fun smoothScrollToTop() {
+        binding.recyclerviewImage.smoothScrollToPosition(0)
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause) called")
+        super.onPause()
+        model.saveState()
+    }
+    override fun onResume() {
+        Log.d(TAG, "onResume) called")
+        super.onResume()
+        model.loadState()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
