7e39e6e028fe8d00e8a864ec6ca2c97d9d63f898
sunjoolee <sheisthepoem@naver.com>
2024-04-14 05:16:46 +0900

    Feat: Add shared preference managers
    Used Moshi Library

---
+++ app/src/main/java/com/sparta/imagesearch/pref_util/KeywordPrefManager.kt
@@ -0,0 +1,18 @@
+package com.sparta.imagesearch.pref_util
+
+import com.sparta.imagesearch.MyApplication
+
+object KeywordPrefManager {
+    private const val KEYWORD_PREF_NAME = "keyword_shared_preferences"
+    private const val KEYWORD_PREF_KEY = "keyword_pref_key"
+
+    fun loadKeyword(): String{
+        val pref = MyApplication.appContext!!.getSharedPreferences(KEYWORD_PREF_NAME, 0)
+        return pref.getString(KEYWORD_PREF_KEY, "") ?: ""
+    }
+
+    fun saveKeyword(keyWord:String){
+        val pref = MyApplication.appContext!!.getSharedPreferences(KEYWORD_PREF_NAME, 0)
+        pref.edit().putString(KEYWORD_PREF_KEY, keyWord).commit()
+    }
+}
---
+++ app/src/main/java/com/sparta/imagesearch/pref_util/SavedItemPrefManager.kt
@@ -0,0 +1,67 @@
+package com.sparta.imagesearch.pref_util
+
+import com.sparta.imagesearch.MyApplication
+import com.sparta.imagesearch.data.Folder
+import com.sparta.imagesearch.data.Item
+import com.squareup.moshi.JsonDataException
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+
+object SavedItemPrefManager {
+    private const val SAVED_ITEM_PREF_NAME = "saved_item_shared_preferences"
+    private const val SAVED_ITEM_PREF_KEY = "saved_item_pref_key"
+
+    private val pref = MyApplication.appContext!!.getSharedPreferences(SAVED_ITEM_PREF_NAME, 0)
+
+    private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    private val type = Types.newParameterizedType(List::class.java, String::class.java)
+
+    fun moveSavedItem(item: Item, destFolderId: String) {
+        saveSavedItems(loadSavedItems().map {
+            if (it.id == item.id) it.copy(folderId = destFolderId) else it
+        })
+    }
+
+    fun deleteSavedItem(item: Item) {
+        saveSavedItems(loadSavedItems().filterNot { it.id == item.id })
+    }
+
+    fun deleteSavedItemsInFolder(folderId: String) {
+        saveSavedItems(loadSavedItems().filterNot { it.folderId == folderId })
+    }
+
+    fun loadSavedItems(): List<Item> {
+        val savedItems = mutableListOf<Item>()
+        try {
+            moshi.adapter<List<String>>(type).fromJson(
+                pref.getString(SAVED_ITEM_PREF_KEY, "") ?: ""
+            )?.forEach { serializedItem ->
+                moshi.adapter(Item::class.java).fromJson(serializedItem)?.let {
+                    savedItems.add(it)
+                }
+            }
+        }catch (e:JsonDataException){
+            e.printStackTrace()
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+        return savedItems.toList()
+    }
+
+    fun saveSavedItems(savedItems: List<Item>) {
+        val serializedSavedItems = savedItems
+            .filterNot {
+                it.folderId == Folder.NO_FOLDER_ID
+            }
+            .map {
+                moshi.adapter(Item::class.java).toJson(it)
+            }
+
+        pref.edit().putString(
+            SAVED_ITEM_PREF_KEY,
+            moshi.adapter<List<String>>(type).toJson(serializedSavedItems)
+        ).commit()
+    }
+}
