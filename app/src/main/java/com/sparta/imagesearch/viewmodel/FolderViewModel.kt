4b3d0d582c1f013f0b47826d33bf840c2a273afb
sunjoolee <sheisthepoem@naver.com>
2024-04-14 19:09:11 +0900

    Refactor: Refactor FolderFragment to MVVM

--- app/src/main/java/com/sparta/imagesearch/viewmodel/FolderViewModel.kt
+++ app/src/main/java/com/sparta/imagesearch/viewmodel/FolderViewModel.kt
@@ -1,42 +1,102 @@
 package com.sparta.imagesearch.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sparta.imagesearch.data.Folder
+import com.sparta.imagesearch.data.FolderId
 import com.sparta.imagesearch.data.Item
 import com.sparta.imagesearch.pref_util.FolderPrefManager
 import com.sparta.imagesearch.pref_util.SavedItemPrefManager
 import com.sparta.imagesearch.view.folder.FolderModel
 
 class FolderViewModel : ViewModel() {
+    private val TAG = "FolderViewModel"
 
-    private val _folderModels = MutableLiveData<List<FolderModel>>()
-    val folderModels: LiveData<List<FolderModel>> get() = _folderModels
-
-    private val _selectedFolderId = MutableLiveData<String>(FolderModel.DEFAULT_FOLDER_ID)
+    private val _selectedFolderId = MutableLiveData<String>(FolderId.DEFAULT_FOLDER.id)
     val selectedFolderId: LiveData<String> get() = _selectedFolderId
 
-    private val _folderItems = MutableLiveData<List<Item>>()
-    val folderItems: LiveData<List<Item>> get() = _folderItems
+    private val _folderModels = MutableLiveData<List<FolderModel>>(emptyList())
+    val folderModels: LiveData<List<FolderModel>> get() = _folderModels
 
     private val _resultFolderModels = MediatorLiveData<List<FolderModel>>().apply {
         addSource(folderModels) { folderModels ->
+            Log.d(TAG, "_resultFolderModels.onChange) folderModels changed")
             value = folderModels.map { it.copy(isSelected = (it.id == selectedFolderId.value!!)) }
         }
-        addSource(selectedFolderId) { selectedFolderId ->
-            value = folderModels.value?.map { it.copy(isSelected = (it.id == selectedFolderId)) }
+        addSource(selectedFolderId) { selectedFolderId  ->
+            Log.d(TAG, "_resultFolderModels.onChange) selectedFolderId changed")
+            loadItemsInFolder()
+            value = folderModels.value!!.map { it.copy(isSelected = (it.id == selectedFolderId)) }
         }
     }
     val resultFolderModels: LiveData<List<FolderModel>> get() = _resultFolderModels
 
-    init {
-        _folderModels.value = listOf(Folder.getDefaultFolder().convert())
-        _selectedFolderId.value = Folder.DEFAULT_FOLDER_ID
+    private val _itemsInFolder = MutableLiveData<List<Item>>(emptyList())
+    val itemsInFolder: LiveData<List<Item>> get() = _itemsInFolder
 
+    private fun loadFolders() {
+        _folderModels.value = FolderPrefManager.loadFolders().map { it.convert() }
+        if(folderModels.value!!.isEmpty())
+            _folderModels.value = listOf(Folder.getDefaultFolder().convert().copy(isSelected = true))
+        Log.d(TAG, "loadFolders) folderModels.size: ${folderModels.value!!.size}")
+    }
+
+    private fun saveFolders() {
+        Log.d(TAG, "saveFolders) folderModels.size: ${folderModels.value!!.size}")
+        FolderPrefManager.saveFolders(folderModels.value!!.map { it.convert() })
+    }
+
+    fun selectFolder(folderModel: FolderModel) {
+        _selectedFolderId.value = folderModel.id
+    }
+
+    fun deleteFolders(deleteFolderIdList: List<String>) {
+        if (deleteFolderIdList.contains(selectedFolderId.value))
+            _selectedFolderId.value = FolderId.DEFAULT_FOLDER.id
+
+        _folderModels.value = folderModels.value!!.filterNot {
+            deleteFolderIdList.contains(it.id)
+        }
+    }
+
+    fun addFolder(name: String, colorHex: String) {
+        val newFolder = Folder(name = name, colorHex = colorHex)
+        _folderModels.value = _folderModels.value!! + listOf(newFolder.convert())
+    }
+
+    fun moveFolder(item: Item, destFolderId: String) {
+        if (destFolderId == item.folderId) return
+        SavedItemPrefManager.moveSavedItem(item, destFolderId)
+
+        _itemsInFolder.value = _itemsInFolder.value!!.filterNot { it.id == item.id }
+    }
+
+    private fun loadItemsInFolder() {
+        Log.d(TAG, "loadItemsInFolder) called")
+        _itemsInFolder.value = SavedItemPrefManager.loadSavedItems()
+            .filter { it.folderId == selectedFolderId.value!! }
+    }
+    private fun saveItemsInFolder(){
+        Log.d(TAG, "saveItemsInFolder) called")
+        SavedItemPrefManager.filterSavedItems(folderModels.value!!.map { it.id })
+    }
+
+    fun unSaveItem(item: Item) {
+        SavedItemPrefManager.deleteSavedItem(item)
+        _itemsInFolder.value = _itemsInFolder.value!!.filterNot { it.id == item.id }
+    }
+
+    fun loadState() {
         loadFolders()
-        loadFolderItems()
+        loadItemsInFolder()
+    }
+
+    fun saveState() {
+        saveFolders()
+        saveItemsInFolder()
     }
 
     private fun Folder.convert() =
@@ -44,52 +104,4 @@ class FolderViewModel : ViewModel() {
 
     private fun FolderModel.convert() =
         Folder(id, name, colorHex)
-
-    private fun loadFolders() {
-        _folderModels.value = FolderPrefManager.loadFolders().map { it.convert() }
-    }
-
-    private fun loadFolderItems() {
-        val savedItems = SavedItemPrefManager.loadSavedItems()
-        _folderItems.value = savedItems.filter { it.folderId == selectedFolderId.value!! }
-    }
-
-    fun selectFolder(folderModel: FolderModel) {
-        _selectedFolderId.value = folderModel.id
-        loadFolderItems()
-    }
-
-    fun deleteFolders(folderIdList:List<String>){
-        folderIdList.forEach { deleteFolder(it) }
-        if(folderIdList.contains(selectedFolderId.value))
-            _selectedFolderId.value = FolderModel.DEFAULT_FOLDER_ID
-    }
-
-    private fun deleteFolder(folderId: String) {
-        if (folderId == FolderModel.DEFAULT_FOLDER_ID) return
-
-        SavedItemPrefManager.deleteSavedItemsInFolder(folderId)
-        FolderPrefManager.deleteFolder(folderId)
-
-        _folderModels.value = _folderModels.value?.filterNot { it.id == folderId } ?: emptyList()
-    }
-
-    fun addFolder(name: String, colorHex: String) {
-        val newFolder = Folder(name = name, colorHex = colorHex)
-        _folderModels.value =
-            _folderModels.value ?: emptyList<FolderModel>() + listOf(newFolder.convert())
-        FolderPrefManager.saveFolders(folderModels.value!!.map { it.convert() })
-    }
-
-    fun unSaveItem(item:Item){
-        SavedItemPrefManager.deleteSavedItem(item)
-        _folderItems.value = _folderItems.value?.filterNot { it == item } ?: emptyList()
-    }
-
-    fun moveFolder(item:Item, destFolderId:String){
-        if(destFolderId == item.folderId) return
-
-        SavedItemPrefManager.moveSavedItem(item, destFolderId)
-        _folderItems.value = _folderItems.value?.filterNot { it == item } ?: emptyList()
-    }
 }
