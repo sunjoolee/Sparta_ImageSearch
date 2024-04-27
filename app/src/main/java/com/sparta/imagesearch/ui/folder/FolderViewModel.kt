6b303d913357346a9c98b504117c2b6f85556df3
sunjoolee <sheisthepoem@naver.com>
2024-04-28 08:12:16 +0900

    Refactor: Refactor FolderViewModel
    App crashing when showing dialog...

--- app/src/main/java/com/sparta/imagesearch/ui/folder/FolderViewModel.kt
+++ app/src/main/java/com/sparta/imagesearch/ui/folder/FolderViewModel.kt
@@ -5,13 +5,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.sparta.imagesearch.data.Item
+import com.sparta.imagesearch.data.repository.SavedItemRepository
 import com.sparta.imagesearch.data.source.local.folder.Folder
 import com.sparta.imagesearch.data.source.local.folder.FolderId
-import com.sparta.imagesearch.data.repository.Item
 import com.sparta.imagesearch.data.source.local.folder.FolderPrefManager
-import com.sparta.imagesearch.data.source.local.savedItem.SavedItemPrefManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class FolderViewModel : ViewModel() {
+@HiltViewModel
+class FolderViewModel @Inject constructor(
+    private val savedItemRepository: SavedItemRepository
+) : ViewModel() {
     private val TAG = "FolderViewModel"
 
     private val _selectedFolderId = MutableLiveData<String>(FolderId.DEFAULT_FOLDER.id)
@@ -25,7 +30,7 @@ class FolderViewModel : ViewModel() {
             Log.d(TAG, "_resultFolderModels.onChange) folderModels changed")
             value = folderModels.map { it.copy(isSelected = (it.id == selectedFolderId.value!!)) }
         }
-        addSource(selectedFolderId) { selectedFolderId  ->
+        addSource(selectedFolderId) { selectedFolderId ->
             Log.d(TAG, "_resultFolderModels.onChange) selectedFolderId changed")
             loadItemsInFolder()
             value = folderModels.value!!.map { it.copy(isSelected = (it.id == selectedFolderId)) }
@@ -38,8 +43,9 @@ class FolderViewModel : ViewModel() {
 
     private fun loadFolders() {
         _folderModels.value = FolderPrefManager.loadFolders().map { it.convert() }
-        if(folderModels.value!!.isEmpty())
-            _folderModels.value = listOf(Folder.getDefaultFolder().convert().copy(isSelected = true))
+        if (folderModels.value!!.isEmpty())
+            _folderModels.value =
+                listOf(Folder.getDefaultFolder().convert().copy(isSelected = true))
         Log.d(TAG, "loadFolders) folderModels.size: ${folderModels.value!!.size}")
     }
 
@@ -59,6 +65,8 @@ class FolderViewModel : ViewModel() {
         _folderModels.value = folderModels.value!!.filterNot {
             deleteFolderIdList.contains(it.id)
         }
+
+        savedItemRepository.deleteFolderSavedItems(deleteFolderIdList)
     }
 
     fun addFolder(name: String, colorHex: String) {
@@ -68,23 +76,20 @@ class FolderViewModel : ViewModel() {
 
     fun moveFolder(item: Item, destFolderId: String) {
         if (destFolderId == item.folderId) return
-        SavedItemPrefManager.moveSavedItem(item, destFolderId)
+        savedItemRepository.moveSavedItem(item.id, destFolderId)
 
         _itemsInFolder.value = _itemsInFolder.value!!.filterNot { it.id == item.id }
     }
 
     private fun loadItemsInFolder() {
         Log.d(TAG, "loadItemsInFolder) called")
-        _itemsInFolder.value = SavedItemPrefManager.loadSavedItems()
-            .filter { it.folderId == selectedFolderId.value!! }
-    }
-    private fun saveItemsInFolder(){
-        Log.d(TAG, "saveItemsInFolder) called")
-        SavedItemPrefManager.filterSavedItems(folderModels.value!!.map { it.id })
+        _itemsInFolder.value =
+            savedItemRepository.loadFolderSavedItems(selectedFolderId.value!!)
+
     }
 
     fun unSaveItem(item: Item) {
-        SavedItemPrefManager.deleteSavedItem(item)
+        savedItemRepository.deleteSavedItem(item)
         _itemsInFolder.value = _itemsInFolder.value!!.filterNot { it.id == item.id }
     }
 
@@ -95,7 +100,6 @@ class FolderViewModel : ViewModel() {
 
     fun saveState() {
         saveFolders()
-        saveItemsInFolder()
     }
 
     private fun Folder.convert() =
