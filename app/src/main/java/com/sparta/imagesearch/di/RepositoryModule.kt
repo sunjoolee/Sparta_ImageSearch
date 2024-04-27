c51309e835448222369d257f4a31077ca26e599e
sunjoolee <sheisthepoem@naver.com>
2024-04-28 08:10:37 +0900

    Feat: Add binding in Module for saved item repository

--- app/src/main/java/com/sparta/imagesearch/di/RepositoryModule.kt
+++ app/src/main/java/com/sparta/imagesearch/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.sparta.imagesearch.di
 
 import com.sparta.imagesearch.data.repository.ItemRepository
 import com.sparta.imagesearch.data.repository.ItemRepositoryImpl
+import com.sparta.imagesearch.data.repository.SavedItemRepository
+import com.sparta.imagesearch.data.repository.SavedItemRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -12,4 +14,7 @@ import dagger.hilt.android.components.ViewModelComponent
 abstract class RepositoryModule {
     @Binds
     abstract fun bindsItemRepository(repositoryImpl: ItemRepositoryImpl): ItemRepository
+
+    @Binds
+    abstract fun bindsSavedItemRepository(repositoryImpl: SavedItemRepositoryImpl): SavedItemRepository
 }
