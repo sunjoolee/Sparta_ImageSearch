0e64db88e0081fe56df677f572408e383ceafdf2
sunjoolee <sheisthepoem@naver.com>
2024-04-27 20:28:25 +0900

    Refactor: Change date format function to String extension

--- app/src/main/java/com/sparta/imagesearch/util/Extensions.kt
+++ app/src/main/java/com/sparta/imagesearch/util/Extensions.kt
@@ -1,5 +1,10 @@
 package com.sparta.imagesearch.util
 
 import android.content.res.Resources
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun Float.fromDpToPx():Int = (this * Resources.getSystem().displayMetrics.density).toInt()
+fun String.formatDate(): String = LocalDateTime
+    .parse(this, DateTimeFormatter.ISO_DATE_TIME)
+    .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
