99ee1519d515bb616e7d3289b0091de95140210c
sunjoolee <sheisthepoem@naver.com>
2024-05-13 14:57:55 +0900

    [Feat] Feat grid cell config

--- app/src/main/java/com/sparta/imagesearch/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/sparta/imagesearch/presentation/search/SearchScreen.kt
@@ -1,13 +1,18 @@
 package com.sparta.imagesearch.presentation.search
 
+import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridState
@@ -15,22 +20,36 @@ import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.sparta.imagesearch.R
@@ -50,6 +69,12 @@ fun SearchScreen(
     val searchScreenState by viewModel.state.collectAsState(initial = SearchScreenState())
     val searchScreenInputs = viewModel.inputs
 
+    val gridCellConfiguration =
+        if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            StaggeredGridCells.Adaptive(minSize = 128.dp)
+        } else {
+            StaggeredGridCells.Fixed(2)
+        }
     val gridState = rememberLazyStaggeredGridState()
     val showScrollToTopFab by remember {
         derivedStateOf {
@@ -78,12 +103,14 @@ fun SearchScreen(
             modifier = modifier.padding(innerPadding),
         ) {
             ImageSearchBar(
-                onSearch = searchScreenInputs::updateKeyword,
+                modifier = modifier.fillMaxWidth(),
                 query = searchScreenState.keyword,
-                onQueryChange = searchScreenInputs::updateKeyword
+                onQueryChange = searchScreenInputs::updateKeyword,
+                onSearch = searchScreenInputs::updateKeyword
             )
             ResultItemsContent(
                 modifier = modifier.padding(top = 8.dp),
+                gridCells = gridCellConfiguration,
                 gridState = gridState,
                 resultItems = searchScreenState.resultItems,
                 onHeartClick = searchScreenInputs::saveItem
@@ -135,6 +162,7 @@ fun ImageSearchBar(
 @Composable
 fun ResultItemsContent(
     modifier: Modifier = Modifier,
+    gridCells: StaggeredGridCells,
     gridState: LazyStaggeredGridState,
     resultItems: List<Item>,
     onHeartClick: (item: Item) -> Unit
@@ -142,7 +170,7 @@ fun ResultItemsContent(
     Box(modifier = modifier.fillMaxSize()) {
         LazyVerticalStaggeredGrid(
             state = gridState,
-            columns = StaggeredGridCells.Fixed(2),
+            columns = gridCells,
             contentPadding = PaddingValues(16.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalItemSpacing = 8.dp
